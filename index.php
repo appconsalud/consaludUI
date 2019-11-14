7c84a4bd166656251324e6fc2cecf03d6b74728c
conSalud <appconsalud@gmail.com>
2019-11-14 12:41:57 -0300

    se corrigieron errores en la presentacion de los menu, ya se cargan y se cargó el login

--- index.php
+++ index.php
@@ -13,28 +13,39 @@ require_once 'templates/frameworkUI/functions.php';
 
 
 $loader = new \Twig\Loader\FilesystemLoader('templates');
+$loader1 = new \Twig\Loader\FilesystemLoader('templates/panels');
 $twig = new \Twig\Environment($loader, [
-    'cache' => 'compilation_cache',
+   // 'cache' => 'compilation_cache',  DESCOMENTAR CUANDO ESTÉ EN PRODUCCIÓN PARA CREAR CACHE
+], 
+        $loader1, [
+   //Cargar acá lo que se necesite para hacer caché*/
 ]);
+
+
+
+
 $cabecera = "conSalud - Consgresos Medicos";
 $titulo = "Pagina principal";
 
-$cargaMenuUsuario = $menuUsuario;
+//$cargaMenuUsuario = true;
 
 if(sessionAbierta()){
     $cargaMenuUsuario = $menuUsuario;
+    $cargarLogin = false;
 }else{
     $cargaMenuUsuario = null;
+    $cargarLogin = 'panels/login.html';
 }
-
-
 
 echo $twig->render('basic.html', [
     'titulo' => $titulo, 
     'cabecera' => $cabecera, 
     'menu' => $menuPpalSp, 
     'urlHome' => $urlHome,
-    'userMenu' => $cargaMenuUsuario]);
+    'panelMenuUsuario' => 'panels/menuUsuario.html',
+    'menuUsuario' => $menuUsuario,
+    'login' => $cargarLogin,
+    'menuUsuarioCargar' => true]);
 
 
 ?>
